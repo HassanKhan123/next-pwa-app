f09ceb62690082b021524fceb05603fcf294f861
Abdur Rafay <88268344+Arafayyy@users.noreply.github.com>
2024-09-04 15:40:03 +0500

    Initial Commit

---
+++ src/app/page.tsx
@@ -0,0 +1,6 @@
+"use client"
+import Search from "@/components/pages/Search";
+
+export default function Home() {
+  return <Search />
+}
---
+++ src/components/pages/Search.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Search() {
+  return (
+    <div className="flex items-center justify-center min-h-screen">
+    <h1 className="text-white text-3xl">Hello World</h1>
+  </div>
+  );
+}
+
+export default Search;
