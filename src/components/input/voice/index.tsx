8ecc7dd4afd14b4b76d1ee09feeb6a6351b19929
Abdur Rafay <88268344+Arafayyy@users.noreply.github.com>
2024-09-25 12:44:14 +0500

    fix: microphone position on mobile

--- src/components/input/voice/index.tsx
+++ src/components/input/voice/index.tsx
@@ -7,28 +7,20 @@ import { useAtom } from "jotai";
 import { chatDataAtom, historyAtom , loadingAtom} from "@/atoms";
 import { postMessage } from "@/services/api/api";
 import { usePathname } from "next/navigation";
+import useSpeechRecognition from "@/hooks/useSpeechRecognition";
 
 
-declare global {
-  interface Window {
-    webkitSpeechRecognition: any;
-    mozSpeechRecognition: any;
-    msSpeechRecognition: any;
-    SpeechRecognition: any;
-  }
-}
 
 function InputWithVoice() {
   const [searchQuery, setSearchQuery] = useState("");
-  const [isListening, setIsListening] = useState(false);
   const router = useRouter();
   const [, setHistory] = useAtom(historyAtom);
   const [, setChatData] = useAtom(chatDataAtom);
   const [loading, setLoading] = useAtom(loadingAtom)
   const pathname = usePathname()
-  let recognition: any;
-  let finalTranscript = "";
-  let interimTranscript = "";
+  const { isListening, startListening, stopListening } = useSpeechRecognition((transcript: string) => {
+    setSearchQuery(transcript);
+  });
 
   const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setSearchQuery(event.target.value);
@@ -108,69 +100,14 @@ function InputWithVoice() {
     }
   };
 
-  if (typeof window !== 'undefined' && !recognition) {
-    const SpeechRecognition =
-      window.SpeechRecognition ||
-      window.webkitSpeechRecognition ||
-      window.mozSpeechRecognition ||
-      window.msSpeechRecognition;
 
-    if (SpeechRecognition) {
-      recognition = new SpeechRecognition();
-      recognition.lang = "en-US";
-      recognition.interimResults = true;
-      recognition.maxAlternatives = 1;
-      recognition.continuous = true;
-    }
-  }
 
   const handleMicClick = () => {
-    if (!recognition) return;
-  
-    console.log('is listening', isListening);
-    if (!isListening) {
-      setIsListening(true);
-      recognition.start();
-      recognition.onend = () => {
-        if (isListening) {
-          recognition.start();
-          console.log('on end hit');
-        }
-      };
+    if (isListening) {
+      stopListening();
     } else {
-      recognition.stop();
-      console.log('Recording stopped');
-      setIsListening(false);
+      startListening();
     }
-  
-    recognition.onresult = (event: any) => {
-      for (let i = event.resultIndex; i < event.results.length; i++) {
-        const transcript = event.results[i][0].transcript;
-        console.log("transcript", transcript);
-  
-        if (event.results[i].isFinal) {
-          finalTranscript += transcript + " ";
-          setSearchQuery(finalTranscript);
-        } else {
-          interimTranscript += transcript;
-        }
-  
-        console.log("final transcript", finalTranscript);
-        console.log("interim transcript", interimTranscript);
-      }
-    };
-
-    recognition.onerror = (event: any) => {
-      if (event.error === 'not-allowed' || event.error === 'service-not-allowed') {
-        setIsListening(false); 
-      }
-    }
-  
-    setTimeout(() => {
-      console.log('Automatic stop after 15 seconds');
-      recognition.stop();
-      setIsListening(false);
-    }, 15000);
   };
   
 
@@ -185,7 +122,7 @@ function InputWithVoice() {
         disabled={loading}
         className="bg-[#0c1019] font-roboto text-white rounded-[12px] h-[56px] w-full p-[2px_40px_2px_20px] border border-[rgba(255,255,255,0.10)]"
       />
-        <div onClick={handleMicClick} className="absolute right-[65px] cursor-pointer inset-y-0 flex items-center">
+        <div onClick={handleMicClick} className="absolute right-[55px] lg:right-[65px] cursor-pointer inset-y-0 flex items-center">
           <Image src={isListening ? StopIcon : VoiceIcon} alt="voice_icon" />
         </div>
       <div
